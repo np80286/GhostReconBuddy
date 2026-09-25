33e44cc0475546559f3b7439ff561d5eb19d81f4
Nick Paisley <nick@paisleydevelopment.com>
2026-09-25 14:20:09 -0500

    Add sourced weapon reference thumbnails

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const imageConfig = {
   remotePatterns: [
     { protocol: 'https' as const, hostname: 'commons.wikimedia.org' },
     { protocol: 'https' as const, hostname: 'upload.wikimedia.org' },
+    { protocol: 'https' as const, hostname: 'thumb.wikimedia.org' },
   ],
 };
 
